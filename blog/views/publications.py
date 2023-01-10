d7fc38298acd65f1e950322f07d1a1f106dc4cac
nmescv <mescvnv@gmail.com>
2023-01-10 16:53:39 +0300

    Промежуточный коммит - стили

--- blog/views/publications.py
+++ blog/views/publications.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.shortcuts import render
+
 from blog.models import Section, Post, Content
 
 
@@ -8,10 +9,11 @@ def home(request):
 	sections = Section.objects.all()
 	posts = Post.objects.all().order_by('-created_at')[:6]
 	data = {
-			'sections' : sections,
+			'sections'    : sections,
 			'publications': posts
 			}
 	return render(request, 'blog/home.html', data)
+
 
 # def publications_by_section(request, category_id):
 
@@ -28,10 +30,23 @@ def posts(request):
 
 
 def show_post(request, id):
+
+	def show_last_post_by_section(request_post):
+		last_posts_by_section = Post.objects.filter(section=request_post.section).order_by('-created_at')[:4]
+		print(last_posts_by_section)
+		# if request_post in last_posts_by_section:
+		# 	last_posts_by_section.delete(request_post)
+		# 	return last_posts_by_section
+		# else:
+		return last_posts_by_section[:3]
+
 	post = Post.objects.get(id=id)
 	contents = Content.objects.filter(post=post)
+	related_by_section_last_posts = show_last_post_by_section(post)
+
 	data = {
 			'post': post,
+			'related_posts': related_by_section_last_posts,
 			'contents': contents
 			}
 	return render(request, 'blog/post.html', data)
